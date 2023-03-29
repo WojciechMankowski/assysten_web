066b1e736a1a7308778f623f18cc8619d87c9cb9
Wojciech Mankowski <wojtekm510@gmail.com>
2023-03-29 08:25:15 +0200

    add  redirect to login page when user is not login

--- assystent_web_react/src/views/home.tsx
+++ assystent_web_react/src/views/home.tsx
@@ -1,6 +1,6 @@
 import Fields from "../components/fields"
 import Screen from "../components/screen"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { connectToAPIWeather } from "../API/connectToAPI"
 import { connectToAPIOpen } from "../API/connectToAPI"
 import { connectToAPIwWikipedia } from "../API/connectToAPI"
@@ -8,20 +8,28 @@ import { connectToAPIwWikipedia } from "../API/connectToAPI"
 const Home = () => {
 	const [commend, setCommand] = useState("")
 	const [data, setData] = useState({})
+	useEffect(() => {
+		let user_local = localStorage.getItem("user")
+		console.log(user_local === "")
+		if (user_local === "") {
+			window.location.replace("http://localhost:3000/login")
+		}
+	}, [])
+
 	const executeTheCommand = () => {
-        console.log(`test: ${commend.substring(7)}`);
+		console.log(`test: ${commend.substring(7)}`)
 		if (commend.startsWith("pogoda") || commend.startsWith("Pogoda")) {
-            connectToAPIWeather(commend.substring(7), setData)
-		} else if (commend.startsWith("otwórz") || commend.startsWith("Otwórz")) { 
+			connectToAPIWeather(commend.substring(7), setData)
+		} else if (commend.startsWith("otwórz") || commend.startsWith("Otwórz")) {
 			connectToAPIOpen(commend.substring(7), setData)
 		} else if (commend.startsWith("wikipedia") || commend.startsWith("Wikipedia")) {
 			connectToAPIwWikipedia(commend.substring(10), setData)
-		} 
+		}
 	}
 	return (
 		<div className="ome_screen">
 			<Screen command={commend} data={data} />
-			<Fields command={commend} setCommand={setCommand} executeTheCommand={executeTheCommand}/>
+			<Fields command={commend} setCommand={setCommand} executeTheCommand={executeTheCommand} />
 		</div>
 	)
 }
